053fc06f804e9d814df6d9a9f6b3f71911812c97
Shiku03 <162791110+Shiku03@users.noreply.github.com>
2024-06-26 12:36:45 +0000

    Added images to website

---
+++ src/main.js
@@ -0,0 +1,66 @@
+//Grab Elements
+const selectElement = selector=>{
+    const element=document.querySelector(selector)
+    if (element) return element;
+    throw new Error ('Something went wrong, make sure that ${selector} exists or is typed correctly.');
+  
+};
+
+//Nav styles on scroll
+const scrollHeader= () =>{
+    const headerElement= selectElement('#header');
+    if(this.scrollY >=15){
+        headerElement.classList.add('activated');
+    }else{
+        headerElement.classList.remove('activated');
+    }
+};
+window.addEventListener('scroll', scrollHeader);
+//
+//Open menu and search pop-up
+const menuToggleIcon = selectElement('#menu-toggle-icon');
+
+const toggleMenu = () => {
+    const mobileMenu = selectElement('#menu');
+    mobileMenu.classList.toggle('activated');
+    menuToggleIcon.classList.toggle('activated');
+};
+
+menuToggleIcon.addEventListener('click', toggleMenu); 
+
+//Open/Close form popup
+
+const formOpenBtn = selectElement('#search-icon');
+const formCloseBtn = selectElement('#form-close-btn');
+const searchFormContainer = selectElement('#search-form-container');
+
+formOpenBtn.addEventListener('click', () => searchFormContainer.classList.add('activated'));
+formCloseBtn.addEventListener('click', () => searchFormContainer.classList.remove('activated'));
+
+window.addEventListener('keyup', event=>{
+    if(event.key=== 'Escape') searchFormContainer.classList.remove('activated');
+});
+
+///Close the search form popup on ESC keypress
+
+//Switch theme/add to local storage
+const bodyElement=document.body;
+const  themeToggleBtn = selectElement('#theme-toggle-btn');
+const currentTheme= localStorage.getItem('currentTheme');
+
+if(currentTheme){
+    bodyElement.classList.add('light-theme');
+}
+
+
+themeToggleBtn.addEventListener('click', () =>{
+    bodyElement.classList.toggle('light-theme');
+
+    if(bodyElement.classList.contains('light-theme')){
+        localStorage.setItem('currentTheme', 'themeActive');
+    }else{
+        localStorage.removeItem('currentTheme');
+    }
+});
+
+//Swiper
